7fea2eaf35687a7d2c4bbfec1abf3561c4f0530c
root <root@LAPTOP-M8GALNCK.localdomain>
2019-11-12 23:46:33 -0500

    work so far

--- parameters.py
+++ parameters.py
@@ -1,10 +1,9 @@
 # search query 
 search_query = 'site:linkedin.com/in/ AND "university of waterloo" AND "management engineering"'
 
-
 # file were scraped profile information will be stored  
 file_name = 'results_file.csv'
 
 # login credentials
 linkedin_username = 'peaches.hp@gmail.com'
-linkedin_password = 'MSCI446'
+linkedin_password = 'MSCI436'
