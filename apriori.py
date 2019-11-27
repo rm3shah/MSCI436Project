5ebb58b9c32fe9bf4f603d12b40668af9510cd2b
Remi Shah <remi_shah@hotmail.com>
2019-11-26 21:42:34 -0500

    apriori done

--- apriori.py
+++ apriori.py
@@ -1,34 +1,76 @@
 # Import necessary python libraries
 import pandas as pd
-import csv
-#from mlxtend.preprocessing import TransactionEncoder
-#from mlxtend.frequent_patterns import apriori, association_rules
+import numpy as np
+import json
+import random
+import matplotlib.pyplot as plt
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
 
+# read in JSON into array of arrays
+dataset=[]
+with open('train.json') as json_file:
+    data = json.load(json_file)
+    for recipe in data:
+        dataset.append(recipe['ingredients'])
+for recipe in dataset:
+    print(recipe)
 
-#Maybe do this for json instead of csv
-dataset = pd.read_json("../input/train.json")
-dataset['ingredients_clean_string'] = [' , '.join(z).strip() for z in dataset['ingredients']]  
-dataset['ingredients_string'] = [' '.join([WordNetLemmatizer().lemmatize(re.sub('[^A-Za-z]', ' ', line)) for line in lists]).strip() for lists in dataset['ingredients']]       
-
-# Read in CSV file into an array of arrays
-#dataset = []
-#with open('aprioriData1.csv') as f:
-#	reader = csv.reader(f)
-#	for row in reader:
-#		dataset.append(row)
-#for row in dataset: 
-#	print(row)
 
 # Transform your data for the apriori algorithm
-oht = TransactionEncoder()
-oht_ary = oht.fit(dataset).transform(dataset)
-df = pd.DataFrame(oht_ary, columns=oht.columns_)
-print(df)
+encoder = TransactionEncoder()
+arr = encoder.fit(dataset).transform(dataset)
+df = pd.DataFrame(arr, columns=encoder.columns_)
+df
 
-frequent_itemsets = apriori(df, min_support=0.6, use_colnames=True)
-print(frequent_itemsets)
+frequent_itemsets = apriori(df, min_support=0.05, use_colnames=True)
+frequent_itemsets
 
-rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.7)
+rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.2)
 print(rules[['antecedents', 'consequents', 'support', 'confidence']])
 
+support=rules['support'].values
+confidence=rules['confidence'].values
+for i in range (len(support)):
+   support[i] = support[i] + 0.0025 * (random.randint(1,10) - 5) 
+   confidence[i] = confidence[i] + 0.0025 * (random.randint(1,10) - 5)
+ 
+plt.scatter(support, confidence, alpha=0.5, marker="*")
+plt.xlabel('support')
+plt.ylabel('confidence') 
+plt.show()
 
+def draw_graph(rules, rules_to_show):
+  G1 = nx.DiGraph()
+   
+  color_map=[]
+  N = 50
+  colors = np.random.rand(N)       
+   
+  for i in range(rules_to_show): 
+    G1.add_node("R"+str(i))
+    
+    for a in rules.iloc[i]['antecedents']:
+        G1.add_node(a)
+        
+        G1.add_edge(a, "R"+str(i), color=colors[i] , weight = 2)
+       
+    for c in rules.iloc[i]['consequents']:
+             
+            G1.add_node(c)
+            
+            G1.add_edge("R"+str(i), c, color=colors[i],  weight=2)
+   
+  edges = G1.edges()
+  colors = [G1[u][v]['color'] for u,v in edges]
+  weights = [G1[u][v]['weight'] for u,v in edges]
+ 
+  pos = nx.spring_layout(G1, k=16, scale=1)
+  nx.draw(G1, pos, edges=edges, node_color = color_map, edge_color=colors, width=weights, font_size=16, with_labels=False)            
+   
+  for p in pos:  # raise text positions
+           pos[p][1] += 0.07
+  nx.draw_networkx_labels(G1, pos)
+  plt.show()
+
+  draw_graph(rules, 6)
